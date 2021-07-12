ca84ed75cc8955ee132d526ff63eba2f5edc025d
Kenshi Trần <hieupronhat1502@gmail.com>
2021-07-12 14:11:21 +0700

    up date inventory

--- lib/screens/inventory/inventory/inventory.dart
+++ lib/screens/inventory/inventory/inventory.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class Inventory extends StatefulWidget {
   const Inventory({Key? key}) : super(key: key);
-
   @override
   _InventoryState createState() => _InventoryState();
 }
@@ -10,10 +9,32 @@ class Inventory extends StatefulWidget {
 class _InventoryState extends State<Inventory> {
   @override
   Widget build(BuildContext context) {
-    // TODO: Implement Equipped
-    return Container(
-      child: Text('INVENTORY'),
-      alignment: Alignment.center,
+    return ListView.builder(
+      physics: ClampingScrollPhysics(),
+      itemBuilder: (context, position) {
+        //return ListItem(); TODO: lấy ListItem trong database
+        return Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            elevation: 1,
+            color: (position % 2) == 0 ? Colors.blueAccent : Colors.greenAccent,
+            child: ListTile(
+              leading: Icon(Icons.arrow_right),
+              title: Text(
+                'Đồ ${position}',
+                style: TextStyle(color: Colors.purple),
+              ),
+              subtitle: Text('Hp + ...: Atk + ...'),
+              onTap: () {
+                //TODO: hiện thông tin trang bị
+              },
+              onLongPress: () {
+                //TODO: chọn trang bị
+              },
+            ));
+      },
+      itemCount: 50,
     );
   }
 }
